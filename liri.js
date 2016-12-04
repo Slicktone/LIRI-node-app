c37c311f6806e9626e474fc7fa275698d09b3600
TonyUCLA <TonyUCLA>
2016-12-04 14:14:16 -0800

    Twitter and Spotify added

--- liri.js
+++ liri.js
@@ -6,12 +6,21 @@ var fs = require('fs');
 // var randomTxt = process.argv[5]; // not sure what to require
 var userInput = process.argv[2];
 
-// Twitter Stuff
+
+// Twitter Lookup
 if (userInput === "my-tweets") {
-	twitterApp();
+	Twitter();
+}
+// Spotify Lookup
+if (userInput === "spotify-this-song") {
+  Spotify();
+}
+// OMDB Movie Lookup
+if (userInput === "movie-this") {
+  Movie();
 }
 
-function twitterApp () {
+function Twitter () {
 var client = new twitter({
   consumer_key: keyGrab.consumer_key,
   consumer_secret: keyGrab.consumer_secret,
@@ -35,13 +44,49 @@ client.get('favorites/list', params, function(error, tweets, response) {
 }
 
 
-// function spotify(searchSong) {
+function Spotify(songName) {
+// can't get anything except The Sign to show =/
 
-// }
+  if(songName === undefined) {
+    songName = "The Sign, Ace of Base";
+  }
 
-// function movie(movieLookup) {
+  var songQuery = {
+      type: 'track', 
+      query: songName, 
+      limit: 1
+  };
 
-// }
+  spotify.search(songQuery, function(error, data) {
+      if (error) {
+        console.log("Darn! Theres an error: " + error);
+        return;
+       } 
+      //  else {
+      //   console.log(JSON.stringify(data, null, 1));
+      // }
+      var songInfo = data.tracks.items[0];
+      console.log("Artist " + songInfo.artists[0].name);
+      console.log("Song Name: " + songInfo.name);
+      console.log("Preview Link " + songInfo.preview_url);
+      console.log("Album " + songInfo.album.name);
+  })
+}
+
+function Movie(movieLookup) {
+  if(!movieLookup) {
+    movieLookup = "Mr.Nobody";
+  }
+  var queryURL = "http://www.omdbapi.com/";
+
+request(queryURL, function(error, response, data) {
+  if (response.statusCode !== 200) {
+    console.log("Information cannot be displayed because of: " + response.statusCode);
+  }
+  var result = JSON.parse(data);
+  console.log(result);
+})
+}
 
 // function random(backStreetBois) {
 
